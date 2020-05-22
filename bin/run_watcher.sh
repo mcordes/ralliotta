7264a53f7523e3f37b6c63df444af47a8d1074c3
Matt Cordes <cordes.matthew@gmail.com>
2020-05-22 08:17:10 -0400

    fix path

--- bin/run_watcher.sh
+++ bin/run_watcher.sh
@@ -4,5 +4,5 @@
 # TODO-mrc: OSX doesn't have the -r flag for xargs
 #docker exec -it rally2_server_1 /bin/sh -c "ps aux | grep parcel | grep -v grep | awk '{print $1}' | xargs -r kill"
 
-docker exec -it rally2_server_1 /bin/sh -c "cd /app && npm run watch"
+docker exec -it rally-alt_server_1 /bin/sh -c "cd /app && npm run watch"
 
