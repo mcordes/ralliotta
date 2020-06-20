f9b382960618ac8735421023ddfafc4c9d4c2cee
Matt Cordes <cordes.matthew@gmail.com>
2020-06-20 19:56:27 -0400

    wip

---
+++ assets/components/MyWork.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <ItemList v-bind:showMyItemsOnly="true" v-bind:heading="'My work'"/>
+    </div>
+</template>
+
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+    import ItemList from "./ItemList.vue";
+
+    @Component({
+        components: {ItemList},
+    })
+    export default class MyWork extends Vue {
+    };
+
+</script>
+
+
+<style scoped>
+</style>
